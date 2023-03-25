cc878d8715b78b29add3eed0c2c4596468982e5f
mohdashiq18 <moashiq2018@gmail.com>
2023-03-25 11:20:13 +0530

    final-task

--- FrontEnd/task_planner/src/App.js
+++ FrontEnd/task_planner/src/App.js
@@ -9,18 +9,64 @@ function App() {
   const [bug, setBug] = useState(false);
   const [feature, setFeature] = useState(false);
   const [story, setStory] = useState(false);
-  const [assigned, setAssigned] = useState(false);
+  const [assigned, setAssigned] = useState("");
   const [user, setUser] = useState("");
   const [bugDetails, setBugDetails] = useState("");
+  const [featureDetails, setFeatureDetails] = useState("");
+  const [storyDetail, setStoryDetails] = useState("");
   const [data, setData] = useState("");
   const [cheak, setCheak] = useState(false);
   const [id, setId] = useState("");
   useEffect(() => {
     axios
-      .get("http://localhost:8000/get")
+      .get("https://thankful-pumps-eel.cyclic.app/get")
       .then((res) => setData(res.data))
       .catch((err) => console.log(err));
-  }, [bug, cheak,id]);
+  }, [bug, cheak, id, feature, story]);
+
+  const AddStory = () => {
+    const payload = {
+      task: "Story",
+      title: storyDetail,
+      assigned: user || " ",
+      status: false,
+    };
+    if (storyDetail !== "") {
+      axios
+        .post("https://thankful-pumps-eel.cyclic.app/post", payload)
+        .then((res) => {
+          setStory(!story);
+          setFeatureDetails("");
+          setUser("");
+          setAssigned("");
+        })
+        .catch((err) => console.log(err));
+    } else {
+      alert("Please fill Story Details");
+    }
+  };
+
+  const AddFeature = () => {
+    const payload = {
+      task: "Feature",
+      title: featureDetails,
+      assigned: user || " ",
+      status: false,
+    };
+    if (featureDetails !== "") {
+      axios
+        .post("https://thankful-pumps-eel.cyclic.app/post", payload)
+        .then((res) => {
+          setFeature(!feature);
+          setFeatureDetails("");
+          setUser("");
+          setAssigned("");
+        })
+        .catch((err) => console.log(err));
+    } else {
+      alert("Please fill Feature Details");
+    }
+  };
   const AddBug = () => {
     const payload = {
       task: "Bug",
@@ -30,11 +76,12 @@ function App() {
     };
     if (bugDetails !== "") {
       axios
-        .post("http://localhost:8000/post", payload)
+        .post("https://thankful-pumps-eel.cyclic.app/post", payload)
         .then((res) => {
           setBug(!bug);
           setBugDetails("");
           setAssigned("");
+          setUser("");
         })
         .catch((err) => console.log(err));
     } else {
@@ -43,14 +90,20 @@ function App() {
   };
   const assignUpdate = (id) => {
     axios
-      .patch(`http://localhost:8000/update/${id}`, { assigned: assigned })
-      .then((res) => {setId("")
-      setAssigned("")})
+      .patch(`https://thankful-pumps-eel.cyclic.app/update/${id}`, {
+        assigned: assigned,
+      })
+      .then((res) => {
+        setId("");
+        setAssigned("");
+      })
       .catch((err) => console.log(err));
   };
   const statusUpdate = (id, data) => {
     axios
-      .patch(`http://localhost:8000/update/${id}`, { status: !data })
+      .patch(`https://thankful-pumps-eel.cyclic.app/update/${id}`, {
+        status: !data,
+      })
       .then((res) => setCheak(!cheak))
       .catch((err) => console.log(err));
   };
@@ -65,7 +118,7 @@ function App() {
               value={assigned}
               onChange={(e) => setAssigned(e.target.value)}
             />
-            <button onClick={()=>assignUpdate(id)}>update</button>
+            <button onClick={() => assignUpdate(id)}>update</button>
             <button onClick={() => setId("")}>cancel</button>
           </div>
         </div>
@@ -91,6 +144,7 @@ function App() {
               <div className={assigned ? "user" : "hide"}>
                 <input
                   type="text"
+                  value={user}
                   placeholder="Enter Assigned User"
                   onChange={(e) => setUser(e.target.value)}
                 />
@@ -103,7 +157,97 @@ function App() {
                   </button>
                   <button className="button1">
                     <RxCross1
-                      onClick={() => setBug(false)}
+                      onClick={() => {
+                        setUser("");
+                        setBug(false);
+                      }}
+                      className="RxCross1"
+                    />
+                  </button>
+                </div>
+                <div style={{ display: "flex" }}>
+                  {assigned ? "" : user}
+                  <button
+                    className="Assigned"
+                    onClick={() => setAssigned(true)}
+                  >
+                    Assigned
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* hear you can add your task in Bug */}
+          {data &&
+            data.map((item) =>
+              item.task == "Bug" ? (
+                <div id="taskdiv">
+                  <b>Bug Tittle</b>: {item.title}
+                  <div>
+                    <p>
+                      <b>Assigned</b>: {item.assigned}
+                    </p>{" "}
+                    <button onClick={() => setId(item._id)}>
+                      <GrEdit />
+                    </button>
+                  </div>
+                  <div>
+                    <p>
+                      <b>Status</b>:{" "}
+                      <span id={item.status && "span"}>
+                        {item.status ? "Completed" : "Pending"}
+                      </span>
+                    </p>
+                    <button onClick={() => statusUpdate(item._id, item.status)}>
+                      <GrUpdate />
+                    </button>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )
+            )}
+        </div>
+
+        {/* this is feature sprint */}
+        <div className="bug">
+          <div>
+            <div className="sprint">
+              <h2>Feature</h2>
+              <button className="button">
+                <GoPlus
+                  onClick={() => setFeature(!feature)}
+                  className="GoPlus"
+                />
+              </button>
+            </div>
+            <hr className={feature ? "show" : "hide"} id="hr"></hr>
+            <div className={feature ? "show" : "hide"}>
+              <textarea
+                value={featureDetails}
+                placeholder="Enter Features Details"
+                onChange={(e) => setFeatureDetails(e.target.value)}
+              />
+              <div className={assigned ? "user" : "hide"}>
+                <input
+                  type="text"
+                  value={user}
+                  placeholder="Enter Assigned User"
+                  onChange={(e) => setUser(e.target.value)}
+                />
+                <button onClick={() => setAssigned(false)}>Save</button>
+              </div>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <div>
+                  <button className="Add" onClick={AddFeature}>
+                    Add-Task
+                  </button>
+                  <button className="button1">
+                    <RxCross1
+                      onClick={() => {
+                        setFeature(false);
+                        setUser("");
+                      }}
                       className="RxCross1"
                     />
                   </button>
@@ -121,53 +265,39 @@ function App() {
             </div>
           </div>
 
+          {/* hear you can add your task in feature */}
           {data &&
-            data.map((item) => (
-              <div id="taskdiv">
-                <b>Bug Tittle</b>: {item.title}
-                <div>
-                  <p>
-                    <b>Assigned</b>: {item.assigned}
-                  </p>{" "}
-                  <button onClick={() => setId(item._id)}>
-                    <GrEdit />
-                  </button>
+            data.map((item) =>
+              item.task == "Feature" ? (
+                <div id="taskdiv">
+                  <b>Feature Tittle</b>: {item.title}
+                  <div>
+                    <p>
+                      <b>Assigned</b>: {item.assigned}
+                    </p>{" "}
+                    <button onClick={() => setId(item._id)}>
+                      <GrEdit />
+                    </button>
+                  </div>
+                  <div>
+                    <p>
+                      <b>Status</b>:{" "}
+                      <span id={item.status && "span"}>
+                        {item.status ? "Completed" : "Pending"}
+                      </span>
+                    </p>
+                    <button onClick={() => statusUpdate(item._id, item.status)}>
+                      <GrUpdate />
+                    </button>
+                  </div>
                 </div>
-                <div>
-                  <p>
-                    <b>Status</b>:{" "}
-                    <span id={item.status && "span"}>
-                      {item.status ? "Completed" : "Pending"}
-                    </span>
-                  </p>
-                  <button onClick={() => statusUpdate(item._id, item.status)}>
-                    <GrUpdate />
-                  </button>
-                </div>
-              </div>
-            ))}
+              ) : (
+                ""
+              )
+            )}
         </div>
 
-        <div className="bug">
-          <div>
-            <div className="sprint">
-              <h2>Feature</h2>
-              <button className="button">
-                <GoPlus
-                  onClick={() => setFeature(!feature)}
-                  className="GoPlus"
-                />
-              </button>
-            </div>
-            <hr className={feature ? "show" : "hide"} id="hr"></hr>
-            <div className={feature ? "show" : "hide"}>
-              <h3>Create-Task</h3>
-              <textarea />
-              <button>Add-Task</button>
-            </div>
-          </div>
-        </div>
-
+        {/* this is Story sprint */}
         <div className="bug">
           <div>
             <div className="sprint">
@@ -178,11 +308,77 @@ function App() {
             </div>
             <hr className={story ? "show" : "hide"} id="hr"></hr>
             <div className={story ? "show" : "hide"}>
-              <h3>Create-Task</h3>
-              <textarea />
-              <button>Add-Task</button>
+              <textarea
+                value={storyDetail}
+                placeholder="Enter Story Details"
+                onChange={(e) => setStoryDetails(e.target.value)}
+              />
+              <div className={assigned ? "user" : "hide"}>
+                <input
+                  type="text"
+                  value={user}
+                  placeholder="Enter Assigned User"
+                  onChange={(e) => setUser(e.target.value)}
+                />
+                <button onClick={() => setAssigned(false)}>Save</button>
+              </div>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <div>
+                  <button className="Add" onClick={AddStory}>
+                    Add-Task
+                  </button>
+                  <button className="button1">
+                    <RxCross1
+                      onClick={() => {
+                        setStory(false);
+                        setUser("");
+                      }}
+                      className="RxCross1"
+                    />
+                  </button>
+                </div>
+                <div style={{ display: "flex" }}>
+                  {assigned ? "" : user}
+                  <button
+                    className="Assigned"
+                    onClick={() => setAssigned(true)}
+                  >
+                    Assigned
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
+          {/* hear you can add your task in story */}
+          {data &&
+            data.map((item) =>
+              item.task == "Story" ? (
+                <div id="taskdiv">
+                  <b>Story Tittle</b>: {item.title}
+                  <div>
+                    <p>
+                      <b>Assigned</b>: {item.assigned}
+                    </p>{" "}
+                    <button onClick={() => setId(item._id)}>
+                      <GrEdit />
+                    </button>
+                  </div>
+                  <div>
+                    <p>
+                      <b>Status</b>:{" "}
+                      <span id={item.status && "span"}>
+                        {item.status ? "Completed" : "Pending"}
+                      </span>
+                    </p>
+                    <button onClick={() => statusUpdate(item._id, item.status)}>
+                      <GrUpdate />
+                    </button>
+                  </div>
+                </div>
+              ) : (
+                ""
+              )
+            )}
         </div>
       </div>
     </div>
